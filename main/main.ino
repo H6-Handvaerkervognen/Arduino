bc0185d9d16e28fd861f628b922672bb537bb28b
Jakub Grabarski (JAKU0065) <jaku0065@zbc.dk>
2023-04-17 18:54:43 +0200

    Added vibration detection

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+#define pinVibration 34
+int timer = 0;
+int seconds = 5;
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinVibration, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+void detectVibration()
+{
+  int value = analogRead(pinVibration);
+  Serial.println(value);
+  while(value > 2000 && timer != seconds)
+  {
+    delay(1000);
+    value = analogRead(pinVibration);
+    Serial.println(value);
+    timer++;
+    if(timer == seconds)
+    {
+    }
+  }
+  timer = 0;
 }
